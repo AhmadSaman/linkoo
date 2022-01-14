d2b3ae664dce6c19cf738c8316cd3130f6a71ca7
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-01-14 22:29:06 +0300

    Setup Chakra UI Fixes #1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+const colors = {
+  text: "#EEEEEE",
+  Box: "#393E46",
+  background: "#222831",
+  secondary: "#00ADB5",
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Box, Text } from "@chakra-ui/react";
+
+const About: NextPage = () => {
+  return (
+    <Box>
+      <Text color={"text"}>About Page</Text>
+    </Box>
+  );
+};
+
+export default About;
