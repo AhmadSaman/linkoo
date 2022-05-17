42359ecc3acb09f9cb549c5b32916a35f8727168
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-05-17 16:15:04 +0300

    Change Navbar profile to Button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,36 +4,41 @@ import { AiOutlinePlus } from "react-icons/ai";
 const Navbar: React.FC = () => {
   return (
     <Flex paddingY={5} justifyContent={"space-between"} alignItems={"center"}>
-      <Box
-        display={"flex"}
-        color={"text"}
-        padding={"5px"}
-        rounded={"md"}
-        transition={".3s"}
-        _hover={{
-          backgroundColor: "white",
-          rounded: "md",
-          boxSizing: "border-box",
-          cursor: "pointer",
-          transform: "scale(.9)",
-          color: "#222831",
-        }}
-      >
-        <Image
-          src=""
-          rounded={"full"}
-          height={"50px"}
-          fallbackSrc="https://avatars.githubusercontent.com/u/55833403?v=4"
-          alt="Profile Picture"
-        />
-        <Text
-          textStyle={"title"}
-          alignSelf={"center"}
+      <Box>
+        <Button
+          color={"text"}
+          width={"full"}
+          height={"full"}
+          display={"flex"}
+          padding={"5px"}
+          rounded={"md"}
           backgroundColor={"inherit"}
-          marginLeft={"2"}
+          transition={".3s"}
+          _hover={{
+            backgroundColor: "white",
+            rounded: "md",
+            boxSizing: "border-box",
+            cursor: "pointer",
+            transform: "scale(.9)",
+            color: "#222831",
+          }}
         >
-          Ahmad Saman
-        </Text>
+          <Image
+            src=""
+            rounded={"full"}
+            height={"50px"}
+            fallbackSrc="https://avatars.githubusercontent.com/u/55833403?v=4"
+            alt="Profile Picture"
+          />
+          <Text
+            textStyle={"title"}
+            alignSelf={"center"}
+            backgroundColor={"inherit"}
+            marginLeft={"2"}
+          >
+            Ahmad Saman
+          </Text>
+        </Button>
       </Box>
       <Box width={{ md: "10%" }}>
         <Button
