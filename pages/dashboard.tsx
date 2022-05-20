17c270d20977265d1d0008caba59fe26a5425e0d
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-05-20 03:00:14 +0300

    Add Header and Tabs

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,76 @@
-import { Box } from "@chakra-ui/react";
 import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Link,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import LinkNext from "next/link";
 
 const dashboard = () => {
-  return <Box color={"white"}>dashboard</Box>;
+  return (
+    <Container maxW={"1100px"}>
+      <Header />
+      <TabComp />
+    </Container>
+  );
 };
 
+const Header: React.FC = () => {
+  return (
+    <Flex paddingY={5}>
+      <Box
+        backgroundColor={"text"}
+        width={"40px"}
+        height={"40px"}
+        rounded={"full"}
+        alignSelf={"center"}
+        display={"flex"}
+        justifyContent={"center"}
+        transition={".3s"}
+        _hover={{
+          transform: "scale(.9)",
+          cursor: "pointer",
+        }}
+      >
+        <LinkNext href={"/"} passHref>
+          <Link
+            display={"flex"}
+            alignItems={"center"}
+            _active={{ border: "none" }}
+            _focus={{ border: "none" }}
+          >
+            <IoMdArrowRoundBack />
+          </Link>
+        </LinkNext>
+      </Box>
+      <Text fontSize={"4xl"} marginLeft={"5"} color={"text"}>
+        Your Dashboard
+      </Text>
+    </Flex>
+  );
+};
+
+const TabComp: React.FC = () => {
+  return (
+    <Tabs variant="soft-rounded" color={"text"}>
+      <TabList>
+        <Tab _selected={{ bg: "secondary", color: "text" }}>Post</Tab>
+        <Tab _selected={{ bg: "secondary", color: "text" }}>Pending</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel> Posted </TabPanel>
+        <TabPanel>Pending</TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
+};
 export default dashboard;
