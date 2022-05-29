874d1c99734d15bf240f1a7f26de10486b277f9a
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-05-29 22:30:29 +0300

    Create reusable hook for auth

---
+++ hooks/useAuth.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import supabase from "../utils/supabase";
+
+const AuthContext = createContext("Default Value");
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<null | any>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const session = supabase.auth.session();
+    setUser(session?.user ?? null);
+    setLoading(false);
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        setUser(session?.user ?? null);
+        setLoading(false);
+      }
+    );
+    return () => {
+      listener?.unsubscribe();
+    };
+  }, []);
+  const value: any = {
+    signUp: (data: any) => supabase.auth.signUp(data),
+    signIn: (data: any) => supabase.auth.signIn(data),
+    signOut: () => supabase.auth.signOut(),
+    user,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
