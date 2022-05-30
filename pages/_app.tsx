2006f530ea13b3fa77cb751ad285f4c4bb462d30
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-05-30 21:00:25 +0300

    Adding Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource/ibm-plex-sans";
+import { AuthProvider } from "../hooks/useAuth";
 const colors = {
   text: "#EEEEEE",
   box: "#393E46",
@@ -27,9 +28,11 @@ const theme = extendTheme({ colors, fonts, textStyles });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Box background={"background"}>
-        <Component {...pageProps} />
-      </Box>
+      <AuthProvider>
+        <Box background={"background"}>
+          <Component {...pageProps} />
+        </Box>
+      </AuthProvider>
     </ChakraProvider>
   );
 }
