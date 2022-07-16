dc0af4b4445c8c18fcf7737db7ee2806d9b2d79e
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-07-16 11:38:24 +0300

    return all approved data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,35 @@
 import type { NextPage } from "next";
 import { Text, Button, Box, Container } from "@chakra-ui/react";
-// import supabase from "../utils/supabase";
+import supabase from "../utils/supabase";
 import Navbar from "../components/Navbar";
 import { Search } from "../components/Search";
 import { Card } from "../components/Card";
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
+  console.log(posts);
+
   return (
     <Container maxW={"1100px"}>
       <Navbar />
       <Search />
       <Box display={"flex"} flexWrap={"wrap"} justifyContent={"space-evenly"}>
+        {/* <Card />
         <Card />
         <Card />
         <Card />
         <Card />
-        <Card />
-        <Card />
+        <Card /> */}
+        {JSON.stringify(posts)}
       </Box>
     </Container>
   );
 };
-// export async function getStaticProps() {
-//   const { data: posts, error } = await supabase.from("posts").select("*");
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
+export async function getStaticProps() {
+  const { data: posts, error } = await supabase.from("posts").select("*");
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Home;
