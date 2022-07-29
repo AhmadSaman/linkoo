f6e3cfbabd50b577d6f3b7346c8ab791f3fb4fb0
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-07-29 23:19:31 +0300

    Add Search endpoint

--- apis/apis.ts
+++ apis/apis.ts
@@ -6,5 +6,6 @@ const getPosts: () => SupabaseQueryBuilder<any> = () => supabase.from("posts");
 const postPost: (args: object) => PostgrestFilterBuilder<any> = (args: object) =>
   supabase.from("posts").insert(args, { returning: "minimal" });
 const getTags: () => SupabaseQueryBuilder<any> = () => supabase.from("tags");
+const searchPost = (search: string) => supabase.from("posts").select().textSearch("title", `"bun"`);
 
-export { getPosts, getTags, postPost };
+export { getPosts, getTags, postPost, searchPost };
