b1e352f9107bacec397d772f789175bb6400ba1d
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-10-24 19:29:25 +0300

    change the supabase file

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,3 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-export default createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || "", process.env.NEXT_PUBLIC_SUPABASE_KEY || "");
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+export default createClient(supabaseUrl, supabaseAnonKey);
