ab2d7d19dc01f8522234ab8cbb60e90c7a983a11
Ahmad Saman Abdulhadi <ahmadsaman.eng@gmail.com>
2023-01-20 19:49:27 +0300

    fix: typescript version

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,44 +3,29 @@ import { User } from "@supabase/supabase-js";
 import { useUser } from "@supabase/auth-helpers-react";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Container } from "@chakra-ui/react";
+import useSWR from "swr";
 
 import TabsComp from "../components/TabsComp";
 import Header from "../components/DashboardHeader";
+import axios from "axios";
+import { NextPage } from "next";
 
-const Dashboard: React.FC = ({ userData, tags }: any) => {
+const Dashboard: () => false | JSX.Element | null = () => {
+  const { data, error, isLoading } = useSWR("http://localhost:3000/api/dashboard/posts", (url) => axios.get(url));
   const user: User | null = useUser();
+  console.log(data, isLoading);
 
   return (
-    user && (
+    user &&
+    !isLoading && (
       <Container maxW={"1100px"}>
         <Header />
-        {!!tags.length && !!userData.length && <TabsComp postTags={tags} posts={userData} />}
+        {!!data?.data.tags.length && !!data?.data.posts.length && (
+          <TabsComp postTags={data?.data.tags} posts={data?.data.posts} />
+        )}
       </Container>
     )
   );
 };
-
-export async function getServerSideProps(ctx: any) {
-  const supabase = createServerSupabaseClient(ctx);
-  let userData: any = [];
-  let tags: any = [];
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (session) {
-    const { data }: any = await supabase.from("posts").select("*").in("userId", [session?.user.id]);
-    const { data: tag }: any = await supabase.from("tags").select("*");
-    userData = data;
-    tags = tag;
-  }
-
-  return {
-    props: {
-      userData,
-      tags,
-    },
-  };
-}
 
 export default Dashboard;
