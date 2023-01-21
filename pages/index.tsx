65e2edccf87d25cc4fc58ae3582935c6254daaff
Ahmad Saman Abdulhadi <ahmadsaman.eng@gmail.com>
2023-01-21 23:12:20 +0300

    refactor: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
 import type { NextPage } from "next";
-import { Box, Container } from "@chakra-ui/react";
-
-import { Card } from "../components/Card";
-import Navbar from "../components/Navbar";
-import { Search } from "../components/Search";
+import { Box, Container, Spinner } from "@chakra-ui/react";
 import axios from "axios";
 import useSWR from "swr";
 
-type TProps = {
-  serverPosts: object[];
-  tags: object[];
-};
+import { Card } from "../components/Card";
+import Navbar from "../components/Navbar";
+// import { Search } from "../components/Search";
+
 type TUserInfo = {
   name: string;
   avatar: string;
@@ -28,33 +23,32 @@ type TMap = {
   id: number;
 };
 
-const Home: NextPage<TProps> = () => {
-  const { data, error, isLoading } = useSWR("http://localhost:3000/api/index_page", (url) => axios.get(url));
-  // const [posts, setPosts] = useState(serverPosts);
+const Home: NextPage = () => {
+  const { data, isLoading } = useSWR("http://localhost:3000/api", (url) => axios.get(url));
 
   return (
     <Container maxW={"1100px"}>
       <Navbar tags={data?.data.tags} />
       {/* TODO: Search Feature will be improved in the future 🤙 */}
       {/* <Search serverPosts={serverPosts} updatePosts={setPosts} /> */}
-      <Box display={"flex"} flexWrap={"wrap"} justifyContent={"space-evenly"} color={"white"}>
-        {data?.data.serverPosts?.map((value) => {
-          const { title, image, link, description, userInfo, tags: postTags } = value as TMap;
-
-          return (
+      <Box display={"flex"} flexWrap={"wrap"} justifyContent={"space-evenly"} color={"white"} minH={"60vh"}>
+        {isLoading ? (
+          <Spinner size="xl" thickness="4px" speed="0.65s" emptyColor="secondary" color="box" alignSelf={"center"} />
+        ) : (
+          data?.data.serverPosts?.map((value: TMap) => (
             <Card
-              key={link}
-              title={title}
-              image={image}
-              description={description}
-              link={link}
-              userName={userInfo?.name}
-              userImage={userInfo?.avatar}
+              key={value.link}
+              title={value.title}
+              image={value.image}
+              description={value.description}
+              link={value.link}
+              userName={value.userInfo?.name}
+              userImage={value.userInfo?.avatar}
               tags={data?.data.tags}
-              postTags={postTags}
+              postTags={value.tags}
             />
-          );
-        })}
+          ))
+        )}
       </Box>
     </Container>
   );
