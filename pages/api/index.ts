e5dc1adc0c18041546c9b12369a438db360bdf05
Ahmad Saman Abdulhadi <ahmadsaman.eng@gmail.com>
2023-01-22 20:34:53 +0300

    enhance: reverse coming posts

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,9 +3,7 @@ import { PostgrestResponse } from "@supabase/supabase-js";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = { posts: any[] | null; tags: any[] | null };
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const supabase = createServerSupabaseClient({ req, res });
 
   const { data: posts }: PostgrestResponse<any[] | null> = await supabase
@@ -13,5 +11,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     .select()
     .in("approved", ["true"]);
   const { data: tags }: PostgrestResponse<any[] | null> = await supabase.from("tags").select();
-  res.status(200).json({ posts, tags });
+  const reversePosts = posts?.reverse();
+  res.status(200).json({ posts: reversePosts, tags });
 }
