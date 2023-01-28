0c33e15697b567f4015b401e73907a36ce91f29b
Ahmad Saman Abdulhadi <ahmadsaman.eng@gmail.com>
2023-01-28 23:42:04 +0300

    fix: delete index type api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -6,11 +6,8 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const supabase = createServerSupabaseClient({ req, res });
 
-  const { data: posts }: PostgrestResponse<any[] | null> = await supabase
-    .from("posts")
-    .select()
-    .in("approved", ["true"]);
-  const { data: tags }: PostgrestResponse<any[] | null> = await supabase.from("tags").select();
+  const { data: posts } = await supabase.from("posts").select().in("approved", ["true"]);
+  const { data: tags } = await supabase.from("tags").select();
   const reversePosts = posts?.reverse();
   res.status(200).json({ posts: reversePosts, tags });
 }
